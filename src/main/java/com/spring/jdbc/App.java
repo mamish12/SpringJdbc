0ba4ba8e28c3872a5cdd465948548bafabfb9590
mamish12 <mku20sahu@gmail.com>
2023-12-06 17:27:39 +0530

    spring jdbc completed

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -1,8 +1,10 @@
 package com.spring.jdbc;
 
+import java.util.List;
 import java.util.Scanner;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -18,32 +20,35 @@ public class App {
 		int x;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("My jdbc program  stareted...");
-		ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+		//with xml
+//		ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
 
+		//without xml
+		ApplicationContext context=new AnnotationConfigApplicationContext(JdbcConfigWithoutXml.class);
 		StudentDao dao = context.getBean("studentDao", StudentDao.class);
 //insert
-//        Student student=new Student();
-//        do {
-//        	 System.out.println("Enter Id:");
-//             int id=sc.nextInt();
-//             student.setId(id);
-//            
-//             System.out.println("Enter Name:");
-//             String name=sc.nextLine();
-//             name=sc.nextLine();
-//             student.setName(name);
-//             
-//             System.out.println("Enter City:");
-//             String city=sc.next();
-//             student.setCity(city);
-//             System.out.println("please enter 1->Entered Value otherwise exit program");
-//             x=sc.nextInt();
-//        	
-//        }while(x==1);
-//        
-//        int result=dao.insert(student); 
-//        System.out.println("Student Added Successfully..."+result);
-//        
+        Student student=new Student();
+        do {
+        	 System.out.println("Enter Id:");
+             int id=sc.nextInt();
+             student.setId(id);
+            
+             System.out.println("Enter Name:");
+             String name=sc.nextLine();
+             name=sc.nextLine();
+             student.setName(name);
+             
+             System.out.println("Enter City:");
+             String city=sc.next();
+             student.setCity(city);
+             System.out.println("please enter 1->Entered Value otherwise exit program");
+             x=sc.nextInt();
+        	
+        }while(x==1);
+        
+        int result=dao.insert(student); 
+        System.out.println("Student Added Successfully..."+result);
+        
 
 		//update
 //        Student student=new Student();
@@ -55,14 +60,32 @@ public class App {
 //        System.out.println("Student Updated Successfully..."+result);
 
 	//delete
-		System.out.println("Enter the id who want to delete?:");
-		int id=sc.nextInt();
+//		System.out.println("Enter the id who want to delete?:");
+//		int id=sc.nextInt();
+//		
+//		int res=dao.delete(id);
+//		if(res==1)
+//		System.out.println("Data Deleted Successfully..."+"rows:"+res);
+//		else
+//			System.err.println("Id is not present in given table.please pass valid id !!!");
 		
-		int res=dao.delete(id);
-		if(res==1)
-		System.out.println("Data Deleted Successfully..."+"rows:"+res);
-		else
-			System.err.println("Id is not present in given table.please pass valid id !!!");
+		// select single data
+//		do {
+//			System.out.println("Enter the id who want to detail?");
+//			int id=sc.nextInt();
+//			Student student=dao.getStudent(id);
+//			System.out.println(student);
+//			System.out.println("1 to continue or any other enter to exit");
+//			x=sc.nextInt();
+//		}while(x==1);
+//		System.out.println("Exit Successfully !!");
+//		
+		// get all data
+		List<Student> students=dao.getAllStudents();
+		for(Student s:students)
+		{
+			System.out.println(s);
+		}
 		
 	}
 }
--- src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
+++ src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
@@ -1,6 +1,9 @@
 package com.spring.jdbc.dao;
 
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.spring.jdbc.entities.Student;
 
@@ -18,24 +21,39 @@ public class StudentDaoImpl implements StudentDao {
 	public int insert(Student student) {
 		// insert query
 		String query = "insert into student(id,name,city) values(?,?,?)";
-		int result = this.jdbcTemplate.update(query,student.getId(),student.getName(),student.getCity());
+		int result = this.jdbcTemplate.update(query, student.getId(), student.getName(), student.getCity());
 		return result;
 	}
 
 	public int update(Student student) {
 		// updating data
-		String query="update student set name=?,city=? where id=?";
-		int result=this.jdbcTemplate.update(query,student.getName(),student.getCity(),student.getId());
+		String query = "update student set name=?,city=? where id=?";
+		int result = this.jdbcTemplate.update(query, student.getName(), student.getCity(), student.getId());
 		return result;
 	}
 
 	public int delete(int id) {
 		// delete data
-		String query="delete from student where id=?";
-		int id1=this.jdbcTemplate.update(query,id);
+		String query = "delete from student where id=?";
+		int id1 = this.jdbcTemplate.update(query, id);
 		return id1;
-		
+
 	}
-	
+
+	public Student getStudent(int studentId) {
+		// selecting single student data
+		String query = "select * from student where id=?";
+		RowMapper<Student> rowMapper = new RowMapperImpl();
+		Student student = this.jdbcTemplate.queryForObject(query, rowMapper, studentId);
+		return student;
+	}
+
+	public List<Student> getAllStudents() {
+		// selecting multiple students
+		String sql = "select * from student";
+		RowMapper<Student> rowMapper = new RowMapperImpl();
+		List<Student> students =this.jdbcTemplate.query(sql, rowMapper);
+		return students;
+	}
 
 }
